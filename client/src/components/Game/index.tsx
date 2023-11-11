e81590c5998b1545751f7728ad513526b2b05cf5
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-11-11 21:57:41 +0300

    refactor: game mechanics and components decoupling

--- client/src/components/Game/index.tsx
+++ client/src/components/Game/index.tsx
@@ -1,61 +1,61 @@
 import React, { useEffect, useState } from 'react';
+import Duck from './Duck';
+import { DuckType } from '../../interfaces/Duck';
+import { v4 as uuidv4 } from 'uuid';
+import './game.css';
+
+const possibleDuckies: DuckType[] = [
+  { id: 1, name: 'Предательство', speed: 300 },
+  { id: 2, name: 'Насилие', speed: 450 },
+  { id: 3, name: 'Одиночество', speed: 100 },
+  { id: 4, name: 'Темнота', speed: 150 },
+  { id: 5, name: 'Новые места', speed: 300 },
+  { id: 6, name: 'Непривычное, что подрывает ожидания', speed: 200 },
+  { id: 7, name: 'Боязнь быть осмеянным за жизнь в детском доме, боязнь выделяться', speed: 300 },
+  { id: 8, name: 'Взрослые люди', speed: 300 },
+  { id: 9, name: 'Боязнь, что мама исчезнет', speed: 300 },
+  { id: 10, name: 'Боязнь, что органы опеки отберут', speed: 300 },
+  { id: 11, name: 'Врачи', speed: 300 },
+]
+
 
 const Game: React.FC = () => {
   const [score, setScore] = useState(0);
-  const [ducks, setDucks] = useState<{id: number, x: number, y: number}[]>([]);
+  const [ducks, setDucks] = useState<React.ReactElement[]>([]);
   const [HEIGHT, WIDTH] = [400, 600];
-  const DUCK_SPAWN_STEP_MS = 1000;
-  const DUCK_MOVE_STEP_MS = 700;
-  const TRANSITION_MS = 1600;
 
 
-  const handleShoot = (duckId: number) => {
-    setScore((prevScore) => prevScore + 1);
-    setDucks((prevDucks) => prevDucks.filter((duck) => duck.id !== duckId));
+  const handleFrag = () => {
+    setScore(current => current + 1);
   };
 
-  useEffect(() => {
-    const duckSpawnLoop = setInterval(() => {
-      setDucks((prevDucks) => [
-        ...prevDucks,
+  useEffect(()=>{
+    setDucks(
+      possibleDuckies.map(duck => 
         {
-          id: Date.now(),
-          x: WIDTH/2,
-          y: 0,
-        },
-      ]);
-    }, DUCK_SPAWN_STEP_MS);
-    
-    const duckMoveLoops = setInterval(() => {
-      setDucks((prevDucks) =>
-        prevDucks.map((duck) => ({
-          ...duck,
-          x: (duck.x + Math.random() * WIDTH) % WIDTH,
-          y: (duck.y + Math.random() * HEIGHT) % HEIGHT,
-        }))
-      );
-    }, DUCK_MOVE_STEP_MS);
-  
-  
-    return () => {
-      clearInterval(duckMoveLoops);
-      clearInterval(duckSpawnLoop);
-    };
-  }, [HEIGHT, WIDTH]);
+          const uId = uuidv4();
+          return (
+            <Duck 
+              key={uId} uId={uId}
+              handleFrag={handleFrag} 
+              duck={duck} 
+              x={(Math.random() * WIDTH) % WIDTH} 
+              y={(Math.random() * HEIGHT) % HEIGHT}
+            />
+          )
+        }
+      )
+    )
+  }, [HEIGHT, WIDTH])
 
   return (
     <div className="Game">
       <h1>Game</h1>
       <p>Score: {score}</p>
       <div className="game-container">
-        {ducks.map((duck) => (
-          <div
-            key={duck.id}
-            className="duck"
-            style={{ top: `${duck.y}px`, left: `${duck.x}px` , transition: `${TRANSITION_MS}ms`}}
-            onMouseDown={() => handleShoot(duck.id)}
-          />
-        ))}
+        {
+          ducks
+        }
       </div>
     </div>
   );
