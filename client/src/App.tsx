bd619dbe74fbaf4211b87946093aebfe25a072fc
Kurbonov P <kroritord@bk.ru>
2023-11-12 00:44:37 +0300

    update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import StartMenu from './components/startMenu.js'
+import ResultsWindow from './components/ResultsWindow.js'
+import FearsDescription from './components/FearsDescription.js'
 
 function App() {
-  const [count, setCount] = useState(0);
   const [somedata, setSomedata] = useState();
-
   useEffect(()=>{
     async function getSomethingFromBack() {
       const backendUrl = 'http://localhost:9090/api';
@@ -16,33 +15,18 @@ function App() {
     }
     getSomethingFromBack();
   }, [])
+  let WindowCheck = 0;
+  let window;
+if (WindowCheck === 0) {
+  window = <StartMenu/>;
+} else if (WindowCheck === 1) {
+  window = <FearsDescription/>;
+} else {
+  window = <ResultsWindow state={WindowCheck}/>;
+}
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <div>
-          { somedata ? somedata : 'loading...' }
-        </div>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    window
   )
 }
 
