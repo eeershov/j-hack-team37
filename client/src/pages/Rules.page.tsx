38d06bfd35ac90d1faf21bedd23a6092b56b7b83
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-11-16 12:09:17 +0300

    refactor: layout fixes in Rules Page

--- client/src/pages/Rules.page.tsx
+++ client/src/pages/Rules.page.tsx
@@ -1,24 +1,29 @@
+import { useEffect } from 'react';
 import girlMainMenu from '../assets/girlMainMenu.svg';
 import BoyMainMenu from '../assets/BoyMainMenu.svg';
 import header from '../assets/header.svg';
 import '../App.css';
-import background from '../assets/background.svg';
 import Menu from '../components/Menu';
 
 const StartMenu = function () {
-    return (
-        <>
-        <img src={background} className="background" alt="bg" />
-        <img src={header} className="header" alt="header" />  
-        <img src={BoyMainMenu} className="boyMenu" alt="boyMenu" />
-        <img src={girlMainMenu} className="girlMenu" alt="girlMenu" />
-        <span className='startMenu'>
-            <h2 className="title">Мини-игра <br/> Страхи приемных детей</h2>
-            <h3 className="rules">Одолейте все страхи как можно быстрее, <br/> за каждый побеждённый страх вам начисляются очки. </h3>
-            <Menu/>
-        </span>
-        </>
-    );
+
+  return (
+    <div className='rules-page'>
+      <div className='background-images-block'>
+        <img src={header} className='header-image' alt="header" />
+        <div className='header-boyngirl'>
+          <img src={BoyMainMenu} className="header-illustration boyMenu" alt="boyMenu" />
+          <img src={girlMainMenu} className="header-illustration girlMenu" alt="girlMenu" />
+        </div>
+      </div>
+      <div className='title-block'>
+        <h2 className='title'>Мини-игра</h2>
+        <h2 className="title">Страхи приемных детей</h2>
+        <p className="rules">Одолейте все страхи как можно быстрее, <br /> за каждый побеждённый страх вам начисляются очки. </p>
+      </div>
+      <Menu />
+    </div>
+  );
 }
 
 export default StartMenu;
