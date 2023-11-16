ec8d40f0e8d3dfc1fd450001ed092f2464d7a253
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-11-16 12:09:43 +0300

    refactor: layout of Fears Page

--- client/src/pages/FearsDescription.page.tsx
+++ client/src/pages/FearsDescription.page.tsx
@@ -1,21 +1,27 @@
-import bg from '../assets/background.svg';
-import fears from '../assets/fears.svg';
-import leftFearsGirl from '../assets/leftFearsGirl.svg';
-import rightFearsGirl from '../assets/rightFearsGirl.svg';
+// import leftFearsGirl from '../assets/leftFearsGirl.svg';
+// import rightFearsGirl from '../assets/rightFearsGirl.svg';
 import Menu from '../components/Menu';
+import { possibleDucks } from '../components/Game/possibleDucks';
 
 const FearsDescription = function () {
 
-    return (
-        <div>
-            <img src={bg} className="background" alt="bg" />
-            <h2 className="title">Описание страхов</h2>
-            <img src={fears} className="fears" alt="fears" />
-            <img src={leftFearsGirl} className="leftFearGirl" alt="leftfg" />
-            <img src={rightFearsGirl} className="rightFearGirl" alt="rightfg" />
-            <Menu/>
-        </div>
-    );
+  return (
+    <div className='fears-page'>
+      <h2 className="title">Описание страхов</h2>
+      <ul className='fears-list'>
+        {possibleDucks.map(duck => {
+          const MonsterIcon = duck.monsterIcon;
+          return (
+            <li>
+              <MonsterIcon width={64} height={64} />
+              <p>{duck.name}</p>
+            </li>
+          )
+        })}
+      </ul>
+      <Menu />
+    </div>
+  );
 }
 
 export default FearsDescription;
