8578fb05d2e1f4c7ea6d3f2ee27c69868481af22
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-11-18 10:52:01 +0300

    refactor: styles

--- client/src/pages/FearsDescription.page.tsx
+++ client/src/pages/FearsDescription.page.tsx
@@ -7,20 +7,22 @@ import BackgroundImages from '../components/BackgroundImages';
 const FearsDescription = function () {
 
   return (
-    <div className='fears-page'>
-      <BackgroundImages LeftImg={leftFearsGirl} RightImg={rightFearsGirl} />
-      <h2 className="title">Описание страхов</h2>
-      <ul className='fears-list'>
-        {possibleDucks.map(duck => {
-          const MonsterIcon = duck.monsterIcon;
-          return (
-            <li>
-              <MonsterIcon width={64} height={64} />
-              <p>{duck.name}</p>
-            </li>
-          )
-        })}
-      </ul>
+    <div className='content fears-page'>
+      <main>
+        <BackgroundImages LeftImg={leftFearsGirl} RightImg={rightFearsGirl} />
+        <h2 className="title">Описание страхов</h2>
+        <ul className='fears-list'>
+          {possibleDucks.map(duck => {
+            const MonsterIcon = duck.monsterIcon;
+            return (
+              <li>
+                <MonsterIcon width={64} height={64} />
+                <p>{duck.name}</p>
+              </li>
+            )
+          })}
+        </ul>
+      </main>
       <Menu />
     </div>
   );
--- client/src/pages/Rules.page.tsx
+++ client/src/pages/Rules.page.tsx
@@ -8,13 +8,15 @@ import BackgroundImages from '../components/BackgroundImages';
 const StartMenu = function () {
 
   return (
-    <div className='rules-page'>
-      <BackgroundImages HeaderImg={header} LeftImg={BoyMainMenu} RightImg={girlMainMenu} />
-      <div className='title-block'>
-        <h2 className='title'>Мини-игра</h2>
-        <h2 className="title">Страхи приемных детей</h2>
-        <p className="rules">Одолейте все страхи как можно быстрее, <br /> за каждый побеждённый страх вам начисляются очки. </p>
-      </div>
+    <div className='content rules-page'>
+      <main>
+        <BackgroundImages HeaderImg={header} LeftImg={BoyMainMenu} RightImg={girlMainMenu} />
+        <div className='title-block'>
+          <h2 className='title'>Мини-игра</h2>
+          <h2 className="title">Страхи приемных детей</h2>
+          <p className="rules">Одолейте все страхи как можно быстрее, <br /> за каждый побеждённый страх вам начисляются очки. </p>
+        </div>
+      </main>
       <Menu />
     </div>
   );
