94a9f83e91c796a56b1b5b16954dd86fb0b13036
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2023-11-18 10:55:53 +0300

    fix: bg moved

--- client/src/pages/Results.page.tsx
+++ client/src/pages/Results.page.tsx
@@ -1,4 +1,3 @@
-import bg from '../assets/background.svg';
 import resultBoy from '../assets/resultBoy.svg';
 import resultGirl from '../assets/resultGirl.svg';
 import header from '../assets/header.svg';
@@ -14,13 +13,12 @@ const ResultsWindow = function () {
 
     return (
         <div>
-            <img src={bg} className="background" alt="bg" />
-            <img src={header} className="header result-header" alt="header" /> 
-            <h2 className="title title-result">ХОРОШАЯ РАБОТА! 
-            <br/>КОЛИЧЕСТВО ОЧКОВ: {gameScore} <br/>ВРЕМЯ: {timeToHuman(gameTime)}</h2>
+            <img src={header} className="header result-header" alt="header" />
+            <h2 className="title title-result">ХОРОШАЯ РАБОТА!
+                <br />КОЛИЧЕСТВО ОЧКОВ: {gameScore} <br />ВРЕМЯ: {timeToHuman(gameTime)}</h2>
             <img src={resultBoy} className="leftFearGirl" alt="leftfg" />
             <img src={resultGirl} className="rightFearGirl" alt="rightfg" />
-            <Menu/>
+            <Menu />
         </div>
     );
 }
