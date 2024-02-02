ad7edfe08ffdc60437ab2b7fd6397665a8fedebd
Evgeniy Ershov <38592029+eeershov@users.noreply.github.com>
2024-02-02 15:08:16 +0300

    build: configured base url

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'j-hack-team37',
   server: {
     host: true,
     port: 5173
